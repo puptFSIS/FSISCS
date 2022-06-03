0091caafa99fa3eee6a58d00b785811118c3d7bb
fernan faranal <fernannagrampa@gmail.com>
2022-06-04 01:37:41 +0800

    working email ang galing pre

--- protected/views/administrator/dtr_send_email.php
+++ protected/views/administrator/dtr_send_email.php
@@ -3,58 +3,84 @@
 	// puptipcr2021 - email pass
 	require ('PHPMailer-master/src/PHPMailer.php');
 	require ('PHPMailer-master/src/SMTP.php');
+	include ('config.php');
+
+	$sql = "SELECT DISTINCT tbl_evaluationfaculty.`Email`
+		FROM tbl_evaluationfaculty
+		INNER JOIN tbl_schedule
+		ON tbl_evaluationfaculty.`FCode` = tbl_schedule.`sprof`
+		LEFT JOIN tbl_dtr 
+		ON tbl_dtr.`FCode` = tbl_schedule.`sprof`
+		WHERE tbl_schedule.`sem` = 2 and tbl_schedule.`schoolYear` = '2021-2022' and tbl_dtr.`FCode` IS NULL;";
+		 $result=mysqli_query($conn,$sql);
+		 $count = mysqli_num_rows($result); 
+		 // echo $count;
+	if(!empty($result))
+	{	
+		$new_var = [];
+		$counter = 0;
+		foreach($result as $newresult)
+		{
+			$new_var[] = $newresult['Email'];
+
+			$counter++;
+			
 
 
-	if(isset($_GET['email']))
-	{
-		$email = $_GET['email'];
-		echo $email;
-		die;
+		}
+		for ($i=0; $i <$count ; $i++) { 
+			$email=$new_var[$i];
+			$mailTo = $email;
+			// $body = $_POST['message'];
+			$body = "GUMANA NA BIMB TULOG NA TAYO HAHAHAHAH";
 
+
+			$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+			//$mail->SMTPDebug = 3;	//Logs 
+
+			$mail->isSMTP(); //Disable or comment this in Live server
+
+			$mail->Host = "smtp.gmail.com";	//Host Server
+
+			$mail->SMTPAuth = true;	//To require username and pass
+
+			$mail->Username="FSISipcr2021@gmail.com";
+			$mail->Password="sngcsaxssuoohfiq";
+
+			$mail->SMTPSecure = "tls"; //Either SSL or TLS
+
+			$mail->Port = "587"; //TLS default port
+
+			$mail->From = "FSISipcr2021@gmail.com"; //Where email came from
+			$mail->FromName = "PUP Taguig - Head of Academic Program(HAP)"; // Name of Email sender
+
+			$mail->addAddress($mailTo, "Faculty"); //To where you will send the email
+
+			$mail->isHTML(true); //Enable HTML to atelast design the content
+
+
+			$mail->Subject = "Individual Performance, Commitment and Review (IPCR)"; //Name of Email
+			$mail->Body = $body;
+			$mail->AltBody = "To view the message, please use an HTML compatible email viewer.";
+			
+			if(!$mail->send())
+			{
+				header("location: index.php?r=administrator/ListOfFac&msg=Email could not be sent.&msgType=err");
+			} 
+			else
+			{
+				header("location: index.php?r=administrator/ListOfFac&msg=Email has been sent.&msgType=succ");
+			}
+		}
+	
+	
+	
+	
+		
 	
 
-	$mailTo = "enail";
-	// $body = $_POST['message'];
-	$body = "hello";
-
-
-	$mail = new PHPMailer\PHPMailer\PHPMailer();
-
-	//$mail->SMTPDebug = 3;	//Logs 
-
-	$mail->isSMTP(); //Disable or comment this in Live server
-
-	$mail->Host = "smtp.gmail.com";	//Host Server
-
-	$mail->SMTPAuth = true;	//To require username and pass
-
-	$mail->Username="FSISipcr2021@gmail.com";
-	$mail->Password="sngcsaxssuoohfiq";
-
-	$mail->SMTPSecure = "tls"; //Either SSL or TLS
-
-	$mail->Port = "587"; //TLS default port
-
-	$mail->From = "FSISipcr2021@gmail.com"; //Where email came from
-	$mail->FromName = "PUP Taguig - Head of Academic Program(HAP)"; // Name of Email sender
-
-	$mail->addAddress($mailTo, "Faculty"); //To where you will send the email
-
-	$mail->isHTML(true); //Enable HTML to atelast design the content
-
-
-	$mail->Subject = "Individual Performance, Commitment and Review (IPCR)"; //Name of Email
-	$mail->Body = $body;
-	$mail->AltBody = "To view the message, please use an HTML compatible email viewer.";
 	
-	if(!$mail->send())
-	{
-		header("location: index.php?r=administrator/ListOfFac&msg=Email could not be sent.&msgType=err");
-	} 
-	else
-	{
-		header("location: index.php?r=administrator/ListOfFac&msg=Email has been sent.&msgType=succ");
-	}
 
 }
 	

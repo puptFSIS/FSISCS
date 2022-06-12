fdabbb6c877a098a0b414c6465ac2d2c31a2e3fd
Joshua <joshuadelrosario17@gmail.com>
2022-06-12 13:27:37 +0800

    delete js file

--- js/preventback.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    function disableBack() { window.history.forward() }
-
-    window.onload = disableBack();
-    window.onpageshow = function(evt) { if (evt.persisted) disableBack() }
-});
